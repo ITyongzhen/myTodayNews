85b2c363b79e2b1a31b7ec620f101088e2df7f78
yongzhen <eagle_01yz@163.com>
2018-09-08 15:16:21 +0800

    设置tabbar， 图片资源

--- myTodayNews/myTodayNews/MyTabBarController.swift
+++ myTodayNews/myTodayNews/MyTabBarController.swift
@@ -12,24 +12,35 @@ class MyTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tabbar = UITabBar.appearance()
+        tabbar.tintColor = UIColor(red: 245/255.0, green: 93/255.0, blue: 90/255.0, alpha: 1)
+        
+        addChildViewControllers()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func addChildViewControllers(){
+        setChildViewController(HomeViewController(), title: "首页", imageName: "home_tabbar_32x32_", selectedImageName: "home_tabbar_press_32x32_")
+         setChildViewController(VideoViewController(), title: "视频", imageName: "video_tabbar_32x32_", selectedImageName: "video_tabbar_press_night_32x32_")
+         setChildViewController(HuoshanViewController(), title: "小视频", imageName: "huoshan_tabbar_32x32_", selectedImageName: "huoshan_tabbar_press_32x32_")
+         setChildViewController(MineViewController(), title: "我的", imageName: "mine_tabbar_32x32_", selectedImageName: "mine_tabbar_press_32x32_")
     }
-    */
+    
+    // 初始化子控制器
+    func setChildViewController(_ childController: UIViewController,title: String, imageName: String, selectedImageName: String) {
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: selectedImageName)
+        childController.title = title
+         // 添加导航控制器为 TabBarController 的子控制器
+        let navVC = MyNavigationController(rootViewController: childController)
+        addChildViewController(navVC)
+        
+    }
+
 
 }
